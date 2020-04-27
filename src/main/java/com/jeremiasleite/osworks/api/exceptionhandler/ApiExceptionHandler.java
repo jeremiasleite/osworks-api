b4c1702b62ce7ece51b678624b363ab8821fdd27
jeremiasleite <jeremias23456@gmail.com>
2020-04-27 16:13:20 -0300

    first commit

---
+++ src/main/java/com/jeremiasleite/osworks/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,50 @@
+package com.jeremiasleite.osworks.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.jeremiasleite.osworks.domain.exception.NegocioException;
+
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
+	
+	@ExceptionHandler(NegocioException.class)
+	public ResponseEntity<Object> handleNegocio(NegocioException ex, WebRequest request) {
+		var status  = HttpStatus.BAD_REQUEST;
+		var problema = new Problema();
+		problema.setStatus(status.value());
+		problema.setTitulo(ex.getMessage());
+		problema.setDataHora(LocalDateTime.now());
+		return handleExceptionInternal(ex, problema, new HttpHeaders(), status, request);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		List<String> listErros = new ArrayList<String>();
+		for (ObjectError error: ex.getBindingResult().getAllErrors()) {
+			String campo = ((FieldError) error).getField();
+			listErros.add(campo +" : " +error.getDefaultMessage());
+		}
+		Problema problema = new Problema();
+		problema.setStatus(status.value());
+		problema.setDataHora(LocalDateTime.now());
+		problema.setTitulo("Um ou mais campo está inválido!");
+		problema.setCampos(listErros);
+		return super.handleExceptionInternal(ex, problema, headers, status, request);
+	}
+
+}
---
+++ src/main/java/com/jeremiasleite/osworks/api/exceptionhandler/Problema.java
@@ -0,0 +1,44 @@
+package com.jeremiasleite.osworks.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
+public class Problema {
+	
+	private Integer status;
+	private LocalDateTime dataHora;
+	private String titulo;
+	private List<String> campos;
+	
+	
+	public List<String> getCampos() {
+		return campos;
+	}
+	public void setCampos(List<String> campos) {
+		this.campos = campos;
+	}
+	public Integer getStatus() {
+		return status;
+	}
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	public LocalDateTime getDataHora() {
+		return dataHora;
+	}
+	public void setDataHora(LocalDateTime dataHora) {
+		this.dataHora = dataHora;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
+	
+}
